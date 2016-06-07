50d2d6139f6b2d12e56c96886c56c50705e81734
Tara <ccmien@gmail.com>
2016-06-07 11:11:55 +0800

    Add: download_progress_hook.

---
+++ download_progress_hook.py
@@ -0,0 +1,24 @@
+"""
+	Extract from tensorflow.examples.notmnist
+"""
+
+last_percent_reported = None
+
+def download_progress_hook(count, blockSize, totalSize):
+  """A hook to report the progress of a download. This is mostly intended for users with
+  slow internet connections. Reports every 1% change in download progress.
+  
+  Use with urllib.urlretrieve
+  """
+  global last_percent_reported
+  percent = int(count * blockSize * 100 / totalSize)
+
+  if last_percent_reported != percent:
+    if percent % 5 == 0:
+      sys.stdout.write("%s%%" % percent)
+      sys.stdout.flush()
+    else:
+      sys.stdout.write(".")
+      sys.stdout.flush()
+      
+    last_percent_reported = percent
